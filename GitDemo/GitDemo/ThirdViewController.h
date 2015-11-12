9b20f203fd528a1445a338f07041de184e7b4e09
zyl <zhang.yulong@st-link.com.cn>
2015-11-12 18:18:41 +0800

    ThirdViewController -----name

---
+++ GitDemo/GitDemo/ThirdViewController.h
@@ -0,0 +1,16 @@
+//
+//  ThirdViewController.h
+//  GitDemo
+//
+//  Created by 张玉龙 on 15/11/12.
+//  Copyright © 2015年 zyl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@property (nonatomic, copy ) NSString *name;
+
+
+@end
---
+++ GitDemo/GitDemo/ThirdViewController.m
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.m
+//  GitDemo
+//
+//  Created by 张玉龙 on 15/11/12.
+//  Copyright © 2015年 zyl. All rights reserved.
+//
+
+#import "ThirdViewController.h"
+
+@implementation ThirdViewController
+
+@end
