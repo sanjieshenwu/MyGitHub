60d045aeba21f0794802dad62e4743eae830ff64
zyl <zhang.yulong@st-link.com.cn>
2015-11-12 17:50:10 +0800

    Add project

---
+++ GitDemo/GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by 张玉龙 on 15/11/12.
+//  Copyright © 2015年 zyl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
